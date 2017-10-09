49e7d9a0beb9f9e10b004ad8dbda61ca4b7660cc
gbdavid2 <gbdavid2@users.noreply.github.com>
2017-10-09 21:11:49 +0100

    Initial Commit

---
+++ AimieSmiles/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AimieSmiles
+//
+//  Created by David Garces on 09/10/2017.
+//  Copyright © 2017 David Garces. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
